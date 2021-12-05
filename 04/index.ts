014ecd8017858db754b633442bc29cf07ea6e749
pfdzm <pfdzm@users.noreply.github.com>
2021-12-05 12:48:53 +0100

    initial work day4

---
+++ 04/index.ts
@@ -0,0 +1,15 @@
+import fs from "fs/promises";
+import path from "path";
+
+const readLines = async (fileName: string) =>
+  (await fs.readFile(path.resolve("04", fileName), "utf8"))
+    .split("\n")
+    .filter(Boolean);
+
+const example = readLines("example.txt");
+
+function giantSquid(input: string[]) {
+  return input.forEach((line) => console.log(line));
+}
+
+example.then(giantSquid)
